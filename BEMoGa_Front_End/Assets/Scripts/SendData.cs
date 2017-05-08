836ff768c1eab4918f6c07d83412c77549fa3907
Per-Arne Waaler Stenshagen <per-arne-stenshagen@hotmail.no>
2017-05-08 17:08:04 +0200

    Basic files for Front-End Most of SendData added

---
+++ BEMoGa_Front_End/Assets/Scripts/SendData.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using Utilities;
+
+/* 
+ * Sends .cs data to server.
+ * All URI's must be supplied manually and sent to each field. Example of URI: http://www.serverplacement.no/projectName/api/field
+ * Supports single Integers, Strings and Booleans.
+ * Supports arrays of Integers and Strings.
+ */
+public class SendData : MonoBehaviour {
+
+  /*
+   * Sends a single integer to the server
+   * @Value: value = integer value to be sent
+   * @Value: field = name of field/key
+   * @Value: URI = URL address of API
+   */
+  public void SendInteger(int value, string field, string URI)
+  {
+    //Creates form to be sent to server
+    WWWForm form = new WWWForm();
+
+    //Adds all fields and values
+    form.AddField(field, value.ToString());
+
+    //Creates web request and sends it to RequestDataTransfer
+    UnityWebRequest www = UnityWebRequest.Post(URI, form);
+    StartCoroutine(RequestDataTransfer(www));
+  }
+
+  /*
+   * Sends a single integer to the server
+   * @Value: value = integer array to be sent
+   * @Value: field = name of field/key
+   * @Value: URI = URL address of API
+   */
+  public void SendIntegerArray(int[] value, string[] field, string URI)
+  {
+    //Creates form to be sent to server
+    WWWForm form = new WWWForm();
+
+    //Adds all fields and values
+    for (int i = 0; i > value.Length; i++)
+    {
+      form.AddField(field[i], value[i].ToString());
+    }
+
+    //Creates web request and sends it to RequestDataTransfer
+    UnityWebRequest www = UnityWebRequest.Post(URI, form);
+    StartCoroutine(RequestDataTransfer(www));
+  }
+
+  /*
+   * Sends a single integer to the server
+   * @Value: value = string value to be sent
+   * @Value: field = name of field/key
+   * @Value: URI = URL address of API
+   */
+  public void SendString(string value, string field, string URI)
+  {
+    //Creates form to be sent to server
+    WWWForm form = new WWWForm();
+
+    //Adds all fields and values
+    form.AddField(field, value);
+
+    //Creates web request and sends it to RequestDataTransfer
+    UnityWebRequest www = UnityWebRequest.Post(URI, form);
+    StartCoroutine(RequestDataTransfer(www));
+  }
+
+  /*
+   * Sends a single integer to the server
+   * @Value: value = string array to be sent
+   * @Value: field = name of field/key
+   * @Value: URI = URL address of API
+   */
+  public void SendStringArray(string[] value, string[] field, string URI)
+  {
+    //Creates form to be sent to server
+    WWWForm form = new WWWForm();
+
+
+    for (int i = 0; i > value.Length; i++)
+    {
+      form.AddField(field[i], value[i]);
+    }
+
+    //Creates web request and sends it to RequestDataTransfer
+    UnityWebRequest www = UnityWebRequest.Post(URI, form);
+    StartCoroutine(RequestDataTransfer(www));
+  }
+
+  /*
+   * Sends a single integer to the server
+   * @Value: value = boolean value to be sent
+   * @Value: field = name of field/key
+   * @Value: URI = URL address of API
+   */
+  public void SendBoolean(bool value, string field, string URI)
+  {
+    //Creates form to be sent to server
+    WWWForm form = new WWWForm();
+
+    //Adds all fields and values
+    form.AddField(field, value.ToString());
+
+    //Creates web request and sends it to RequestDataTransfer
+    UnityWebRequest www = UnityWebRequest.Post(URI, form);
+    StartCoroutine(RequestDataTransfer(www));
+  }
+
+  /*
+   * Sends data to server
+   * @Value: www = UnityWebRequest created by functions above
+   */ 
+  private IEnumerator RequestDataTransfer(UnityWebRequest www)
+  {
+    yield return www.Send();
+
+    Debug.Log("Answer from server after transmitting data:\n" + www.downloadHandler.text);
+
+    if (www.error != null)
+    {
+      Debug.Log("Data transmission failed.\nError: " + www.error);
+      yield break;
+    }
+
+    //Try and convert to error object
+    ErrorContainer error = JsonUtility.FromJson<ErrorContainer>(JsonHelper.GetSubObject(www.downloadHandler.text));
+    if (error != null)
+    {
+      //Check that it is not a default object
+      if (error.status > 0)
+        Debug.Log(error.message);
+    }
+  } 
+}
---
+++ BEMoGa_Front_End/Assets/Scripts/Utilities.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Various containers and helpers for data transmissions and receptions.
+ * May need to be updated for each project. Below is merely a suggestion.
+ */ 
+namespace Utilities{
+
+  /**
+    * Container for json error object
+    */
+  [System.Serializable]
+  public class ErrorContainer
+  {
+    //Error type, example "Error"
+    public string name = "Null";
+
+    //code 401 example
+    public int status = -1;
+
+    //Error message
+    public string message = "Null";
+
+    //code 401 example
+    public int statusCode = -1;
+
+    //All caps code
+    public string code = "Null";
+
+    //Stack trace to js code line
+    public string stack = "Null";
+  }
+
+  /**
+    * Container for json login object
+    */
+  [System.Serializable]
+  public class LoginContainer
+  {
+    //Contains token key
+    public string id = "Null";
+
+    //
+    public int ttl = -1;
+
+    //Date the message was created, format {yyyy-mm-ddThh:mm:ss.fffZ}
+    public string created = "Null";
+
+    //Users user id
+    public int userId = -1;
+  }
+
+  public class JsonHelper
+  {
+    /**
+    * Converts json array into object array
+    * @author ffleurey
+    * @see https://forum.unity3d.com/threads/how-to-load-an-array-with-jsonutility.375735/
+    * @param json to turn into array
+    * @returns array of object type
+    */
+    public static T[] getJsonArray<T>(string json)
+    {
+      string newJson = "{ \"array\": " + json + "}";
+      Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+      return wrapper.array;
+    }
+
+    //serializable wrapper class for getJsonArray function
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+      public T[] array;
+    }
+
+    /**
+    * Quick hack for removing object around error object
+    * @param json to get subobject from
+    */
+    public static string GetSubObject(string json)
+    {
+      //Remove "{ "error": " at the start of json
+      json = json.Remove(0, 9);
+
+      //Remove "}" at end of json
+      json = json.Remove(json.Length - 1);
+
+      return json;
+    }
+  }
+}
