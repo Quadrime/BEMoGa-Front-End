8ae51fd05c9dff6088041630e82c240d3edd7209
Per-Arne Stenshagen <per.arne.stenshagen@gmail.com>
2017-05-09 16:25:05 +0200

    Added login of user and handling if this. Altered comments to reflect Unity standard.

---
+++ BEMoGa_Front_End/Assets/Scripts/DataStorage.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilities;
+
+namespace DataStorage
+{
+  public class GlobalData
+  {
+
+    LoginContainer loginInfo = null;
+    private static GlobalData instance;
+
+    /**
+    * Get a reference to the Global Data object.
+    * There exists only one Global Data object and it lasts the entire duration of the application.
+    * @returns a reference to the Global Data object, if it doesn't exist already it will be created.
+    */
+    public static GlobalData Instance
+    {
+      get
+      {
+        if (instance == null)
+        {
+          instance = new GlobalData();
+        }
+        return instance;
+      }
+    }
+
+    /**
+    * Stores loginInfo
+    * @param loginData = data returned from server
+    */
+    public void SetLoginData(LoginContainer loginData)
+    {
+      loginInfo = loginData;
+    }
+
+    /**
+    * Check if the user is logged in
+    * @returns True if logged in, false if not 
+    */
+    public bool IsLoggedIn()
+    {
+      if (loginInfo != null)
+      {
+        return true;
+      }
+      return false;
+    }
+
+    /**
+    * Gets user's access token
+    * @returns Access token, if not logged in returns null
+    */
+    public string GetAccessToken()
+    {
+      if (IsLoggedIn())
+      {
+        return loginInfo.id;
+      }
+
+      return null;
+    }
+
+    /**
+    * Gets user's ID
+    * @returns UserID, if not logged in returns -1
+    */
+    public int GetUserID()
+    {
+      if (IsLoggedIn())
+      {
+        return loginInfo.userId;
+      }
+
+      return -1;
+    }
+
+    /**
+     * Fully erases currently logged in user (useful for logging out)
+     */
+    public void EraseUserLog()
+    {
+      if (IsLoggedIn())
+      {
+        loginInfo.id = null;
+        loginInfo.userId = -1;
+        loginInfo.created = null;
+      }
+      loginInfo = null;
+    }
+  }
+}
--- BEMoGa_Front_End/Assets/Scripts/SendData.cs
+++ BEMoGa_Front_End/Assets/Scripts/SendData.cs
@@ -3,20 +3,19 @@ using UnityEngine;
 using UnityEngine.Networking;
 using Utilities;
 
-/* 
- * Sends .cs data to server.
- * All URI's must be supplied manually and sent to each field. Example of URI: http://www.serverplacement.no/projectName/api/field
- * Supports single values and arrays.
- * To send non-string values, use the .ToString() function when sending them to SendData.
- */
+///<summary>
+/// Sends .cs data to server.
+/// All URI's must be supplied manually and sent to each field. Example of URI: http://www.serverplacement.no/projectName/api/field
+/// Supports single values and arrays.
+/// To send non-string values, use the .ToString() function when sending them to SendData.
+///</summary>
 public class SendData : MonoBehaviour {
 
-  /*
-   * Sends a single value to the server
-   * @Value: value = integer value to be sent
-   * @Value: field = name of field/key
-   * @Value: URI = URL address of API
-   */
+
+   ///<summary>Sends a single value to the server</summary>
+   ///<param name="value">Value to be sent</param>
+   ///<param name="field">Name of field/key</param>
+   ///<param name="URI">URL address of API</param>
   public void SendSingle(string value, string field, string URI)
   {
     //Creates form to be sent to server
@@ -30,12 +29,10 @@ public class SendData : MonoBehaviour {
     StartCoroutine(RequestDataTransfer(www));
   }
 
-  /*
-   * Sends a single integer to the server
-   * @Value: value = integer array to be sent
-   * @Value: field = name of field/key
-   * @Value: URI = URL address of API
-   */
+  ///<summary>Sends a single value to the server</summary>
+  ///<param name="value">Value array to be sent</param>
+  ///<param name="field">Names of field/key</param>
+  ///<param name="URI">URL address of API</param>
   public void SendArray(string[] value, string[] field, string URI)
   {
     //Creates form to be sent to server
@@ -52,21 +49,24 @@ public class SendData : MonoBehaviour {
     StartCoroutine(RequestDataTransfer(www));
   }
 
-  /*
-   * Sends data to server
-   * @Value: www = UnityWebRequest created by functions above
-   */ 
+  /// <summary>Sends data to server and logs in user locally if succesful</summary>
+  /// <param name="www">UnityWebRequest created by functions above</param>
   private IEnumerator RequestDataTransfer(UnityWebRequest www)
   {
+    //Sends request to server and waits for a response (control given back to Unity in the meantime)
     yield return www.Send();
 
     Debug.Log("Answer from server after transmitting data:\n" + www.downloadHandler.text);
 
+    //Error in transfer request
     if (www.error != null)
     {
       Debug.Log("Data transmission failed.\nError: " + www.error);
       yield break;
     }
+
+    //Error status code
+    int statusCode = 0;
 
     //Try and convert to error object
     ErrorContainer error = JsonUtility.FromJson<ErrorContainer>(JsonHelper.GetSubObject(www.downloadHandler.text));
@@ -74,7 +74,7 @@ public class SendData : MonoBehaviour {
     {
       //Check that it is not a default object
       if (error.status > 0)
-        Debug.Log(error.message);
+        statusCode = error.statusCode;
     }
   } 
 }
